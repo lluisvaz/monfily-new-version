38e077e7e55c43abbc41b686563d8235ae267da4
Luis Felipe👋 <lipevaz.lfv@gmail.com>
2026-02-03 23:24:52 -0300

    update

--- api/index.ts
+++
@@ -1,8 +0,0 @@
-import "dotenv/config";
-import { app, appPromise } from "../backend/src/index";
-import type { VercelRequest, VercelResponse } from "@vercel/node";
-
-export default async function handler(req: VercelRequest, res: VercelResponse) {
-  await appPromise;
-  return app(req, res);
-}
